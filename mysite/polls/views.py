3430e620660e134383dc7d39bb67d145334cf0db
jerripat <jerripatterson@gmail.com>
2021-12-20 19:49:24 -0600

    updates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
+def index(request):
+    html = "Welcome to the Polls app, this is your home page."
+    return HttpResponse(html)
